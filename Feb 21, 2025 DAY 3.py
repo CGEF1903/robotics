5f5e8a2e860d6ccc7a4fece5775e57dae9bbaa68
Tomcatter <cgefgaming@gmail.com>
2025-02-21 10:44:49 -0700

    Update Feb 21, 2025 DAY 3.py

--- Feb 21, 2025 DAY 3.py
+++ Feb 21, 2025 DAY 3.py
@@ -1,1 +1,8 @@
-
+# I learned what concatente is. i learned how to make a simple recipe generator.
+print("==== Recipe Generator ====")
+myFood = input("Name a food: ")
+myPlant = input("Name a type of plant: ")
+myCooking = input("Name a method of cooking: ")
+myBurned = input("What word describes burned food?: ")
+myItem = input("Name a DIY item: ")
+print("Tonights Menu", myCooking, myFood, "with", myBurned, myPlant, "on a bed of", myItem)
